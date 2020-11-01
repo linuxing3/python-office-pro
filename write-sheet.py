e7393dea0f0ab043c68fc38667b438ee6bbb7b17
linuxing3 <linuxing3@qq.com>
2020-11-01 13:22:35 -0400

    sample

--- write-sheet.py
+++ write-sheet.py
@@ -6,6 +6,7 @@ new_workbook = xlwt.Workbook()
 data_sheet = new_workbook.add_sheet('data')
 
 for row in range(10):
-  data_sheet.write(0, row, '数据'+ str(row))
+  for col in range(10):
+    data_sheet.write(row, col, '数据'+ str(row) + '---' + str(col))
 
 new_workbook.save(file_path)
